b26e68fe4b04e6f209aabdaf812299720ba17b46
fahimhasandev <fhasan5@wgu.edu>
2025-02-10 14:00:00 -0600

    set badge

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
 const timeElement = document.getElementById("time");
-const currentTime = new Date().toLocaleTimeString()
-timeElement.textContent = `The current time is ${currentTime}`
+const currentTime = new Date().toLocaleTimeString();
+timeElement.textContent = `The current time is ${currentTime}`;
+
+// show text like TIME or NEW
+chrome.action.setBadgeText(
+  {
+    text: "NEW",
+  },
+  () => {
+    console.log(`Finished setting badged text.`);
+  }
+);
