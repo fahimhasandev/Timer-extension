57be1b9884fbf81c9239cf5222227156344e0310
fahimhasandev <fhasan5@wgu.edu>
2025-06-03 14:00:00 -0500

    working on option.js file

--- options.js
+++ options.js
@@ -1,1 +1,8 @@
-console.log('Option page')
+console.log("Option page");
+
+const submitBtn = document.getElementById("save-btn");
+
+submitBtn.addEventListener("click", () => {
+  const nameInput = document.getElementById("name-input");
+  console.log(nameInput.value);
+});
