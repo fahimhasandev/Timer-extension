7fc0beaf367505a16c3eda81601017603223e803
fahimhasandev <fhasan5@wgu.edu>
2025-06-05 14:00:00 -0500

    chorme storage api

--- options.js
+++ options.js
@@ -1,8 +1,14 @@
-console.log("Option page");
-
 const submitBtn = document.getElementById("save-btn");
+const nameInput = document.getElementById("name-input");
 
 submitBtn.addEventListener("click", () => {
-  const nameInput = document.getElementById("name-input");
-  console.log(nameInput.value);
+  const name = nameInput.value;
+
+  chrome.storage.sync.set({ name }).then(() => {
+    console.log("Value is set");
+  });
+
+  chrome.storage.sync.get(["name"]).then((result) => {
+    console.log("Value currently is " + result.key);
+  });
 });
