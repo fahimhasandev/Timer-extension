453669552b8a483f3b9f9fafe82e5ccc6667e8af
fahimhasandev <fhasan5@wgu.edu>
2025-10-01 14:00:00 -0500

    popup.js test

--- popup.js
+++ popup.js
@@ -1,1 +1,3 @@
-const timeElement = document.getElementById('')
+const timeElement = document.getElementById("time");
+const currentTime = new Date().toLocaleTimeString()
+timeElement.textContent = `The current time is ${currentTime}`
